911648a5bcac3616285dc01da9a5718fc00a5259
Duhoon <eclip6@naver.com>
2022-12-19 21:14:19 +0900

    feat: create rawTransaction and request on server

--- controller.js
+++ controller.js
@@ -1,8 +1,11 @@
 const Web3 = require("web3");
+const axios = require("axios");
 require("dotenv").config();
 const provider = process.env.PROVIDER;
 
 const web3 = new Web3(provider);
+
+
 
 // Contract
 const RockCoin = require("./contract.js");
@@ -22,6 +25,42 @@ module.exports = {
         if(!req.body.recipient || !req.body.amount) 
             return res.status(400).send("Require All Parameter for Transfer Token");
 
+        const data = RockCoin.methods.transfer(req.body.recipient,req.body.amount).encodeABI();
+
+        const tx = {
+            from: process.env.CURRENT_ACCOUNT,
+            to: "0xe0C753D9d5Bad3b93BACCBeD5821b6d439B49f22",
+            gas: 24000,
+            data
+        }
+
+        const txSigned = await web3.eth.accounts.signTransaction(tx, process.env.CURRENT_PRIVATEKEY)
+        .catch(err=>{
+            console.log(err);
+            return res.status(500).send("Internal Error");
+        });
+
+        return await axios.post(process.env.PROVIDER, {
+            jsonrpc: "2.0",
+            method: "eth_sendRawTransaction",
+            params: [txSigned.rawTransaction],
+            id: 1
+        })
+        .then(result=>{
+            console.log(result.data);
+            return res.status(202).send(result.data);
+        })
+        .catch(err=>{
+            console.log(err);
+            return res.status(404).send(err);
+        })
+
+        
+
+        /******************************************/
+        /*          Failed Case 2           */
+        /******************************************/
+
         // const isUnlocked = await new web3.eth.personal.unlockAccount(process.env.CURRENT_ACCOUNT, "", 600)
         // .then((result)=>{ 
         //     console.log("Account unlocked!")
@@ -35,7 +74,9 @@ module.exports = {
         // console.log(isUnlocked);
         // if (!isUnlocked) return res.status(500).send("Not available to unlock Account");
 
-        // Failed Case 1.
+        /******************************************/
+        /*          Failed Case 1           */
+        /******************************************/
 
         // const signer = web3.eth.accounts.privateKeyToAccount(process.env.CURRENT_PRIVATEKEY)
         // web3.eth.accounts.wallet.add(signer);
